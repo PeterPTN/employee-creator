49d4c6e7e2819355fc66c14da3d6be937490ce71
Peter Nguyen <peter.p.t.nguyen@gmail.com>
2023-04-03 13:06:24 +1000

    Remove unused imports

--- employee-creator-api/src/main/java/peter/employeecreatorapi/employees/UpdateEmployeeDto.java
+++ employee-creator-api/src/main/java/peter/employeecreatorapi/employees/UpdateEmployeeDto.java
@@ -5,7 +5,6 @@ import java.time.LocalDate;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Past;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
